724b430899726b2f66bbc01a2056161459f3bc19
gc1004002 <connerg@otc.edu>
2025-02-06 08:59:36 -0600

    init

---
+++ Exam 1 - Student Template/Exam 1 - Higher or Lower/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LargestNumberFinder {
+  public class NumberProgram {
+    static void Main(string[] args) {
+      int counter = 0;
+      int largest = -1;
+      int number = 0;
+      string listOfNums = "";
+      for (; ; ) {
+        if (counter >= 10)
+          break;
+
+        try {
+          Console.Write("Enter a non-zero positive integer\n>>");
+          string sNum = Console.ReadLine() ?? "";
+          number = ProcessInput(sNum, ref largest);
+
+          listOfNums += number + " ";
+
+          counter++;
+          Console.WriteLine();
+        } catch (FormatException ex) {
+          Console.WriteLine($"Format Exception : {ex.Message} \n");
+        } catch (Exception ex) {
+          Console.WriteLine($"Exception : {ex.Message} '\n'");
+        }
+      }
+
+      Console.WriteLine("You entered : " + listOfNums);
+      Console.WriteLine("The largest number is : " + largest);
+    }
+
+    public static int ProcessInput(string input, ref int largest) {
+      int holdNum = 0;
+      if (!Int32.TryParse(input, out holdNum))
+        throw new FormatException("Not in the format of an int");
+
+      if (holdNum <= 0)
+        throw new Exception("Number cannot be negative or zero");
+
+      if (holdNum > largest)
+        largest = holdNum;
+
+      return holdNum;
+    }
+  }
+}
